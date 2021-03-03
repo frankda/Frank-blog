3c20909fd15a8a7b9c4f5c1862835777526936ad
frankda <suyunyi@gmail.com>
2021-03-03 22:22:31 +1100

    - Add db context

---
+++ Data/AppFileContext.cs
@@ -0,0 +1,14 @@
+﻿using FrankBlog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrankBlog.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext (DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppFile> File { get; set; }
+    }
+}
---
+++ StartupBase.cs
@@ -0,0 +1,25 @@
+﻿using FrankBlog.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FrankBlog
+{
+    public class StartupBase
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllersWithViews();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
+
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
+        }
+    }
+}
