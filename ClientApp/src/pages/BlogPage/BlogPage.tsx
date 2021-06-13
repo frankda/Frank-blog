f9fee8db774e2752ffecb490359a0020af446856
frankda <suyunyi@gmail.com>
2021-06-13 15:24:55 +1000

    - Update page layout

--- ClientApp/src/pages/BlogPage/BlogPage.tsx
+++ ClientApp/src/pages/BlogPage/BlogPage.tsx
@@ -1,14 +1,12 @@
 import BlogList from 'components/BlogList/BlogList';
 import Navigation from 'components/Navigation/Navigation';
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const BlogPage: React.FC = () => {
   return (
     <>
       <Navigation />
       <BlogList />
-      <FontAwesomeIcon icon="coffee" />
     </>
   );
 };
