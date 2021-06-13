c42e9063c3fe31a2eecfa9da267b1d0feee63b96
frankda <suyunyi@gmail.com>
2021-06-13 15:24:25 +1000

    - Update navigation

--- ClientApp/src/components/Navigation/Navigation.tsx
+++ ClientApp/src/components/Navigation/Navigation.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import './Navigation.scss';
 
 const Navigation: React.FunctionComponent = () => (
   <nav className="navigation">
     <div className="container">
       <div className="row">
-        <div className="col">
+        <div className="col-10 col-md-3">
           <h1>Frank Da</h1>
+        </div>
+        <div className="col-md-9 navigation__links-container">
+          <a className="navigation__link" href="/">Blog</a>
+          <a className="navigation__link" href="/">About me</a>
         </div>
       </div>
     </div>
